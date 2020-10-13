4f22c33573f3891f161e9a6ce0c551e10dee4ef2
Eamon <eamccarron@mta.ca>
2020-10-13 12:35:53 -0300

    Updated code to reflect design changes

---
+++ src/Controller/CardType.java
@@ -0,0 +1,30 @@
+package Controller;
+
+//An enum to hold card types
+public enum CardType {
+    STEP_FORWARD,
+    TURN_LEFT,
+    TURN_RIGHT,
+    BUG,
+    LASER;
+
+    //Return a nicely formatted string, mostly for debugging and testing purposes
+    @Override
+    public String toString() {
+        switch(this){
+            case STEP_FORWARD:
+                return "Step Forward";
+            case TURN_LEFT:
+                return "Turn Left";
+            case TURN_RIGHT:
+                return "Turn Right";
+            case BUG:
+                return "Bug";
+            case LASER:
+                return "Laser";
+            default:
+                //return standard Object toString() by default, but this default can never actually be reached
+                return super.toString();
+        }
+    }
+}
---
+++ src/Controller/TileType.java
@@ -0,0 +1,10 @@
+package Controller;
+
+//An enum to hold Tile types
+public enum TileType {
+    JEWEL,
+    CRATE,
+    STONE_WALL,
+    ICE_WALL,
+    PUDDLE
+}
