901be741e3269f343572e4e4cd9709d14096445e
samchambre <scchambre@mta.ca>
2020-10-28 22:55:06 -0300

    Decreased number of responsibilities of TurtleMaster by moving certain functions and variables to the Turtle class.

--- src/Model/Turtle.java
+++ src/Model/Turtle.java
@@ -1,4 +1,86 @@
 package Model;
 
 public class Turtle {
+
+    int playerID;
+    private int currPosition;
+    private int prevPosition;
+    private final int boardLength = (int)Math.sqrt(Board.BOARD_SIZE);
+    private static final int NORTH = 0;
+    private static final int EAST = 1;
+    private static final int SOUTH = 2;
+    private static final int WEST = 3;
+    private int direction;
+
+    public Turtle(int playerID){
+        this.playerID  = playerID;
+        //Place players clockwise in 4 corners of board
+        switch(this.playerID){
+            case Board.PLAYER_1:
+                currPosition = 0;
+                direction = SOUTH;
+                break;
+            case Board.PLAYER_2:
+                currPosition = 7;
+                direction = SOUTH;
+                break;
+            case Board.PLAYER_3:
+                currPosition = 63;
+                direction = NORTH;
+                break;
+            case Board.PLAYER_4:
+                currPosition = 56;
+                direction = NORTH;
+        }
+    }
+
+    public boolean moveForward(){
+        switch(direction){
+            case NORTH:
+                if (currPosition<boardLength)
+                    return false;
+                currPosition -= boardLength;
+                break;
+            case EAST:
+                if (currPosition%boardLength == 7)
+                    return false;
+                currPosition += 1;
+                break;
+            case SOUTH:
+                if (currPosition >= Board.BOARD_SIZE-boardLength)
+                    return false;
+                currPosition += boardLength;
+                break;
+            case WEST:
+                if (currPosition%boardLength == 0)
+                    return false;
+                currPosition -= 1;
+                break;
+        }
+        return true;
+    }
+
+    public void bug(){
+        currPosition = prevPosition;
+    }
+
+    public void turn(String leftOrRight){
+        int turnDirection = 1;
+        if (leftOrRight.equals("left"))
+            turnDirection = -1;
+        direction = (direction+turnDirection)%4;
+    }
+
+    public int getPosition() {
+        return currPosition;
+    }
+
+    public int getDirection() {
+        return this.direction;
+    }
+
+    public void setPrevPosition(int currPosition){
+        this.prevPosition = currPosition;
+    }
+
 }
--- src/Model/TurtleMaster.java
+++ src/Model/TurtleMaster.java
@@ -2,88 +2,12 @@ package Model;
 import Controller.CardType;
 public class TurtleMaster {
     private int playerNumber;
-    private int currPosition;
-    private int prevPosition;
-    //private Direction direction;
-    private final int boardLength = (int)Math.sqrt(Board.BOARD_SIZE);
-    private CardType[] hand;
-    private static final int NORTH = 0;
-    private static final int EAST = 1;
-    private static final int SOUTH = 2;
-    private static final int WEST = 3;
-    private int direction;
+    Turtle turtle;
 
 
-    private enum Direction{
-        NORTH,
-        EAST,
-        SOUTH,
-        WEST
-    }
-
-    //Return an int array where each index corresponds to the number of a certain kind of cards remaining
-    //use CardType.ordinal() to index by card type
-    public CardType[] getHand(){
-       return hand; 
-    }
-
-    public TurtleMaster(int playerNumber){
-        this.playerNumber  = playerNumber;
-        //Place players clockwise in 4 corners of board
-        switch(this.playerNumber){
-            case Board.PLAYER_1:
-                currPosition = 0;
-                direction = SOUTH;
-                break;
-            case Board.PLAYER_2:
-                currPosition = 7;
-                direction = SOUTH;
-                break;
-            case Board.PLAYER_3:
-                currPosition = 63;
-                direction = NORTH;
-                break;
-            case Board.PLAYER_4:
-                currPosition = 56;
-                direction = NORTH;
-        }
-    }
-
-    public boolean moveForward(){
-        switch(direction){
-            case NORTH:
-                if (currPosition<boardLength)
-                    return false;
-                currPosition -= boardLength;
-                break;
-            case EAST:
-                if (currPosition%boardLength == 7)
-                    return false;
-                currPosition += 1;
-                break;
-            case SOUTH:
-                if (currPosition >= Board.BOARD_SIZE-boardLength)
-                    return false;
-                currPosition += boardLength;
-                break;
-            case WEST:
-                if (currPosition%boardLength == 0)
-                    return false;
-                currPosition -= 1;
-                break;
-        }
-        return true;
-    }
-
-    public void turn(String leftOrRight){
-        int turnDirection = 1;
-        if (leftOrRight.equals("left"))
-            turnDirection = -1;
-        direction = (direction+turnDirection)%4;
-    }
-
-    public void bug(){
-        currPosition = prevPosition;
+    public TurtleMaster(int playerID){
+        turtle = new Turtle(playerID);
+        this.playerNumber = playerID;
     }
 
     //Return false if move is illegal or if there are not enough cards remaining.
@@ -91,17 +15,18 @@ public class TurtleMaster {
         //TODO Implement rotations
         //TODO update hand
         //TODO Check if move is illegal (enough cards?)
-        prevPosition = currPosition ;
+        turtle.setPrevPosition(turtle.getPosition());
         switch(card){
             case STEP_FORWARD:
-                return moveForward();
+                return turtle.moveForward();
             case BUG:
+                turtle.bug();
                 break;
             case TURN_LEFT:
-                turn("left");
+                turtle.turn("left");
                 break;
             case TURN_RIGHT:
-                turn("right");
+                turtle.turn("right");
                 break;
             default:
                 break;
@@ -113,11 +38,11 @@ public class TurtleMaster {
 		return playerNumber;
 	}
 
-	public int getPosition() {
-		return currPosition;
+	public int getTurtlePosition() {
+		return turtle.getPosition();
     }
 
-	public int getDirection() {
-        return this.direction;
+	public int getTurtleDirection() {
+        return turtle.getDirection();
 	}
 }
